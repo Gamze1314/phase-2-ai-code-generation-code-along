609446ac51c967da452f8643f0ac606c7041cffe
gamze <gamzekandara@gmail.com>
2024-02-19 03:21:19 -0500

    Adds Index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+//“Create a function that takes in a list of books and a genre. It should return a new list of books with the specified genre.”
+
+
+ /** A book object representation.
+ * @typedef {Object} Book
+ * @property {string} title - The title of the book.
+ * @property {string} author - The author of the book.
+ * @property {string} genre - The genre of the book.
+ */
+
+
+ /** returns a list of books with the given genre.
+ * @param {string} genre - The genre of books required.
+ * @param {Book[]} books - An array of Book objects.
+ * @returns {Book[]} An array of Book objects matching the specified genre.
+ */
+function filterByGenre(genre, books) {
+    return books.filter((book) => book.genre === genre);
+}
